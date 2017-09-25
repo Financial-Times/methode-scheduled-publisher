3f5a613e40375690c1095472883d3df0ce37e78c
Andrew Watkins <andrew.watkins@ft.com>
2017-09-25 16:52:48 +0100

    Removed some console logging

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// var debug = ENVIRONMENT !== "production";
 const ENVIRONMENT = 'local';
 const path = require("path");
-// const fs = require('fs');
 const request = require('request');
 const METHODE_QUERY_LOID = process.env.METHODE_QUERY_LOID;
 const METHODE_SWTTOKEN = process.env.METHODE_SWTTOKEN;
@@ -17,6 +15,7 @@ const baseRequest = request.defaults({
 });
 
 function authenticateWithMethode() {
+    console.log("Authenticating");
     return new Promise((resolve, reject) => {
         baseRequest.post('/auth/login', {
             form: {
@@ -38,12 +37,14 @@ function authenticateWithMethode() {
 // run a query that returns a list of UUIDs. needs a token passed from authentication
 // need to stay on the same server, so keep the actual host in a cookie ft-backend-hostname=10_112_17_123
 function runQuery(token) {
-    console.log("Run query start with token ",token);
+    console.log("Running Query");
+    // console.log("Run query start with token ",token);
     baseRequest.post({url: '/query/search?id=' + METHODE_QUERY_LOID + '&token=' + token, json: true, encoding: null}, function (err, res, body) {
-        console.log("before numstories");
-        console.log(err);
-        console.log(body);
+        // console.log("before numstories");
+        // console.log(err);
+        // console.log(body);
         var numStories = JSON.stringify(body.data.count);
+        console.log(numStories + " results");
         for (i = 0; i < numStories; i++) {
             var newUUid = JSON.stringify(body.data.items[i].pstate.uuid);
             var newURL = '/object/actions/publish_web?id=' + newUUid.replace(/\"/g,"");
@@ -51,7 +52,7 @@ function runQuery(token) {
             baseRequest.get({url: newURL, json: true, encoding: null}, function (err, res, body) {
                 var msg = err ? err.errno : res.statusMessage;
                 res = res || {};
-                //console.log("Publishing object " + newUUid);
+                console.log("Publishing object " + newUUid);
                 //console.log("Status code returned: ",JSON.stringify(res.statusCode));
                 //console.log("Status message returned: ",JSON.stringify(res.statusMessage));
                 console.log("Publish returned ", JSON.stringify(res.body));
@@ -62,10 +63,6 @@ function runQuery(token) {
 
 function main() {
     console.log("Starting Methode Automated Publisher");
-    console.log(METHODE_API_ROOTPATH);
-    console.log(METHODE_USERNAME);
-    console.log(METHODE_PASSWORD);
-    console.log(METHODE_SWTTOKEN);
     authenticateWithMethode().then(token => runQuery(token)); // authenticate with Methode, when finished, then run Query
 }
 
